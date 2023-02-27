72296cdb2be83ba65bbc26e4bc73cce41e2340d1
jaromoyler31 <jarroyler@gmail.com>
2023-02-27 09:03:08 -0700

    updating user Screen

--- my-app/src/user.js
+++ my-app/src/user.js
@@ -2,20 +2,49 @@ import './user.css';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faSearch
+  faHouse,
+  faArrowRightLong,
+  faArrowLeftLong,
+  faSearch,
+  faCalendar,
+  faA,
+  faPlus,
+  faEnvelope
+  
 } from '@fortawesome/free-solid-svg-icons';
 import logo from "./IMG/Logo.jpg"
 
 function Dashboard() {
     return(
         <>
+            <div className='sidebar' id="mysidebar">
+                <div className="top">
+                    <div className='a'>
+                        <FontAwesomeIcon icon={faHouse} /> <a>Dashbaord</a>
+                    </div>
+                    <div className='a'>
+                        <FontAwesomeIcon icon={faCalendar} /> <a>Calendar</a>
+                    </div>
+                    <div className='a'>
+                        <FontAwesomeIcon icon={faA} /><FontAwesomeIcon icon={faPlus} /> <a>Grades</a>
+                    </div>
+                    <div className='a'>
+                        <FontAwesomeIcon icon={faEnvelope} /> <a>Inbox</a>
+                    </div>
+
+                </div>
+                <div className="bottom">
+                    <FontAwesomeIcon icon={faArrowLeftLong} className="arrow"/>
+                </div>
+            </div>
+
             <div className="topbar">
                 <div className="left">
                     <img src={logo} alt="Logo" className='logo' />
                 </div>
                 <div className="right">
                     <div className='search'>
-                        <FontAwesomeIcon icon={faSearch} className="searchIcon"/>
+                        <FontAwesomeIcon icon={faSearch} className="searchIcon" onClick={closeNav}/>
                         <input className='input' placeholder='Search'></input>
                     </div>
                     
@@ -24,14 +53,22 @@ function Dashboard() {
                 </div>
             </div> 
 
-            <div className='sidebar'>
-                <a>Dashbaord</a>
-                <a>Calendar</a>
-                <a>Grades</a>
-                <a>Inbox</a>
+            <div id="main">
+                
             </div>
         </>
     )
+}
+
+function openNav() {
+    document.getElementById("mysidebar").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+}
+
+
+function closeNav(){
+    document.getElementById("mysidebar").style.width = "0px";
+    document.getElementById("main").style.marginLeft = "0px";
 }
 
 export default Dashboard;
