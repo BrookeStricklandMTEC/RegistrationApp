b8c4e33ae8ed84369aa7703b246d436584b45a2f
jaromoyler31 <jarroyler@gmail.com>
2023-02-27 07:40:54 -0700

    Updating userScreen

--- my-app/src/user.js
+++ my-app/src/user.js
@@ -4,12 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faSearch
 } from '@fortawesome/free-solid-svg-icons';
+import logo from "./IMG/Logo.jpg"
+
 function Dashboard() {
     return(
         <>
             <div className="topbar">
                 <div className="left">
-
+                    <img src={logo} alt="Logo" className='logo' />
                 </div>
                 <div className="right">
                     <div className='search'>
@@ -17,8 +19,17 @@ function Dashboard() {
                         <input className='input' placeholder='Search'></input>
                     </div>
                     
+                    <div className="user">
+                    </div>
                 </div>
             </div> 
+
+            <div className='sidebar'>
+                <a>Dashbaord</a>
+                <a>Calendar</a>
+                <a>Grades</a>
+                <a>Inbox</a>
+            </div>
         </>
     )
 }
